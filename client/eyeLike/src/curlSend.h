e7c803c364c24edb8e0905f4582f9f227752852e
Ming._.Nine9 <mingnine9@soongsil.ac.kr>
2020-05-30 20:58:42 +0900

    curl

--- client/eyeLike/src/curlSend.h
+++ client/eyeLike/src/curlSend.h
@@ -2,6 +2,16 @@
 #define CURL_SEND_H
 
 #include <curl/curl.h>
-#include <string>
+
+#define PORTNUM 8080
+#define DB_URL "ec2-15-164-111-138.ap-northeast-2.compute.amazonaws.com:8080/api/v3/datasave"
+#define CHECK_URL "abcde"
+#define CHECK_MSG "check"
+#define TO_JSON "Content-Type: application/json"
+
+int curlInit(void);
+CURLcode curlSend(int concentration);
+int curlEnd(void);
+CURLcode curlCheckBoundary(void);
 
 #endif
